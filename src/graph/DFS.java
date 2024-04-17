2a38df03498dc4cd824f8f6a493cf265c26db053
sejun2 <qpfjf56@gmail.com>
2024-04-17 18:50:42 +0900

    add bfs, dfsR

--- src/graph/DFS.java
+++ src/graph/DFS.java
@@ -1,29 +1,55 @@
 package graph;
 
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class DFS {
     public static void main(String[] args) {
-        DfsGraph graph = new DfsGraph(9);
+        System.out.println("DFS");
+        DfsGraph graph1 = new DfsGraph(9);
 
-        graph.addEdge(0, 1);
-        graph.addEdge(1, 2);
-        graph.addEdge(1, 3);
-        graph.addEdge(2, 1);
-        graph.addEdge(2, 3);
-        graph.addEdge(2, 4);
-        graph.addEdge(3, 1);
-        graph.addEdge(3, 2);
-        graph.addEdge(3, 4);
-        graph.addEdge(3, 5);
-        graph.addEdge(4, 2);
-        graph.addEdge(4, 3);
-        graph.addEdge(5, 6);
-        graph.addEdge(5, 7);
-        graph.addEdge(6, 8);
+        graph1.addEdge(0, 1);
+        graph1.addEdge(1, 2);
+        graph1.addEdge(1, 3);
+        graph1.addEdge(2, 4);
+        graph1.addEdge(3, 4);
+        graph1.addEdge(3, 5);
+        graph1.addEdge(5, 6);
+        graph1.addEdge(5, 7);
+        graph1.addEdge(6, 8);
 
-        graph.dfs();
+        graph1.dfs();
+
+        System.out.println("DFS Recursive");
+        DfsGraph graph2 = new DfsGraph(9);
+
+        graph2.addEdge(0, 1);
+        graph2.addEdge(1, 2);
+        graph2.addEdge(1, 3);
+        graph2.addEdge(2, 4);
+        graph2.addEdge(3, 4);
+        graph2.addEdge(3, 5);
+        graph2.addEdge(5, 6);
+        graph2.addEdge(5, 7);
+        graph2.addEdge(6, 8);
+
+        graph2.dfsRecursive();
+
+        DfsGraph graph3 = new DfsGraph(9);
+
+        graph3.addEdge(0, 1);
+        graph3.addEdge(1, 2);
+        graph3.addEdge(1, 3);
+        graph3.addEdge(2, 4);
+        graph3.addEdge(3, 4);
+        graph3.addEdge(3, 5);
+        graph3.addEdge(5, 6);
+        graph3.addEdge(5, 7);
+        graph3.addEdge(6, 8);
+
+        System.out.println("BFS");
+        graph3.bfs();
     }
 }
 
@@ -67,6 +93,22 @@ class DfsGraph {
         dfs(0);
     }
 
+    void dfsRecursive() {
+        dfsRecursive(nodes[0]);
+    }
+
+    void dfsRecursive(Node node) {
+        node.isVisited = true;
+        System.out.println(node.data);
+
+
+        for (int i = 0; i < node.adjacent.size(); i++) {
+            if (node.adjacent.get(i).isVisited || node.adjacent.get(i).adjacent.isEmpty()) continue;
+
+            dfsRecursive(node.adjacent.get(i));
+        }
+    }
+
     void dfs(int index) {
         Node root = nodes[index];
 
@@ -84,6 +126,32 @@ class DfsGraph {
 
                 n.adjacent.get(i).isVisited = true;
                 stack.push(n.adjacent.get(i));
+            }
+        }
+    }
+
+    void bfs() {
+        bfs(0);
+    }
+
+    void bfs(int index) {
+        Queue<Node> queue = new LinkedList<>();
+
+        Node root = nodes[index];
+
+        queue.add(root);
+        root.isVisited = true;
+
+        while (!queue.isEmpty()) {
+            Node n = queue.poll();
+            System.out.println(n.data);
+
+            for (int i = 0; i < n.adjacent.size(); i++) {
+                if (n.adjacent.get(i).isVisited) continue;
+
+                n.adjacent.get(i).isVisited = true;
+
+                queue.add(n.adjacent.get(i));
             }
         }
     }
