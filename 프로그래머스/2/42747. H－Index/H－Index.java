39a5868d1bc5fcd7b365d629bc82685419547150
sejun <qpfjf56@gmail.com>
2024-12-06 17:33:33 +0900

    [level 2] Title: H-Index, Time: 24.81 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,10 +1,12 @@
 import java.util.*;
-
+import java.math.*;
 class Solution {
     public int solution(int[] citations) {
-        Arrays.sort(citations);
+        int max = 0;
         
-        int max = citations[citations.length -1];
+        for(int cite: citations){
+            max = Math.max(max, cite);
+        }
        
         
         for(int i = max;i>=0; i--){
