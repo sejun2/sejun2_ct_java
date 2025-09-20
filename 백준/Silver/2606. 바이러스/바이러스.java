1680702f4601c7aff05cd7e7722cf952629a7df2
sejun <qpfjf56@gmail.com>
2025-09-21 01:15:13 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,55 +1,67 @@
-import java.util.*;
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[] visited;
-    static boolean[][] graph;
-    static int n, v;
+     public static void main(String[] args) throws IOException {
+        // 1. DFS
+        DFS.solution();
 
-    public static void main(String[] args) throws IOException {
-
-        var br = new BufferedReader(new InputStreamReader(System.in));
-        var bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        n = Integer.parseInt(br.readLine());
-        v = Integer.parseInt(br.readLine());
-
-        visited = new boolean[n + 1];
-        graph = new boolean[n + 1][n + 1];
-
-        for (int i = 0; i < v; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-
-            graph[x][y] = true;
-            graph[y][x] = true;
-        }
-
-        dfs(1);
-
-        int answer = 0;
-
-        for (int i = 0; i < visited.length; i++) {
-            if (visited[i]) answer++;
-        }
-
-        // 시작 지점은 제외해야하기 때문에 answer - 1
-        bw.write(String.valueOf(answer - 1));
-
-        br.close();
-        bw.close();
+        // 2. BFS
     }
 
-    static void dfs(int index) {
-        visited[index] = true;
+    static class DFS {
 
-        for (int i = 1; i < n + 1; i++) {
-            if (visited[i] == true) continue;
+        static void solution() throws IOException {
+            boolean[][] graph;
+            boolean[] visited;
 
-            if (graph[index][i] == true) {
-                dfs(i);
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            int n, v;
+
+            n = Integer.parseInt(br.readLine());
+            v = Integer.parseInt(br.readLine());
+
+            visited = new boolean[n + 1];
+
+            // 그래프 초기화
+            // 1부터 시작하므로 n+1
+            graph = new boolean[n + 1][n + 1];
+            for (int i = 0; i < v; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+
+                graph[x][y] = true;
+                graph[y][x] = true;
+            }
+
+            dfs(1, graph, visited, n);
+
+            int answer = 0;
+
+            for (int i = 0; i < visited.length; i++) {
+                if (visited[i]) answer++;
+            }
+
+            // 시작 노드는 제외하므로 answer - 1
+            bw.write(String.valueOf(answer - 1));
+
+            br.close();
+            bw.close();
+        }
+
+        static void dfs(int index, boolean[][] graph, boolean[] visited, int n) {
+            visited[index] = true;
+
+            for (int i = 1; i <= n; i++) {
+                if (visited[i] == true) continue;
+
+                if (graph[index][i] == true) {
+                    dfs(i, graph, visited, n);
+                }
             }
         }
     }
