47a17cf2adc982539bb34a380f072d97b9baafae
sejun <qpfjf56@gmail.com>
2025-11-26 01:02:24 +0900

    [level 3] Title: 이중우선순위큐, Time: 45.14 ms, Memory: 114 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+        PriorityQueue<Integer> maxPq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minPq = new PriorityQueue<>();
+    public int[] solution(String[] operations) {
+        
+       new PriorityQueue<Integer>(maxPq);
+        
+        for(int i= 0; i< operations.length; i++){
+            if(operations[i].contains("I")){
+                insertNumber(Integer.parseInt(operations[i].substring(2, operations[i].length())));
+            }else if(operations[i].contains("D 1")){
+                removeMax();
+            }else{
+                removeMin();
+            }
+        }
+        
+        if(maxPq.isEmpty()){
+            return new int[]{0 ,0};
+        }
+        return new int[] {maxPq.peek(), minPq.peek()};
+        
+    }
+    
+    void insertNumber(int number){
+       maxPq.add(number);
+       minPq.add(number);
+    }
+    
+    void removeMax(){
+        Integer polled = maxPq.poll();
+        if(polled != null){
+            minPq.remove(polled);
+        }
+    }
+    
+    void removeMin(){
+       Integer polled = minPq.poll(); 
+        if(polled != null){
+            maxPq.remove(polled);
+        }
+    }
+}
